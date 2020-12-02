4bed7b4185a4d057ded3d0d1e1bc1d1c9074a3c8
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2020-12-02 21:50:45 +0400

    Small changes in Day2

--- Algorithms and Data Structures/Advent of code/Day 2/Day2.py
+++ Algorithms and Data Structures/Advent of code/Day 2/Day2.py
@@ -10,8 +10,8 @@ def get_num_of_valid_passcodes(fileName):
     f = open(fileName, "r")
     count = 0
     for line in f:
-        numbers, letter, word = parse_line(line)
-        if (word.count(letter) >= numbers[0]) and (word.count(letter) <= numbers[1]):
+        range_limits, letter, word = parse_line(line)
+        if (word.count(letter) >= range_limits[0]) and (word.count(letter) <= range_limits[1]):
             count += 1
     f.close()
     return count
@@ -21,8 +21,8 @@ def get_num_of_valid_pass_TCAS(fileName):
     f = open(fileName, "r")
     count = 0
     for line in f:
-        numbers, letter, word = parse_line(line)
-        if (word[numbers[0] - 1] == letter) ^ (word[numbers[1] - 1] == letter): # Used exclusive or as our condition is only true when exactly one of them is true
+        indices, letter, word = parse_line(line)
+        if (word[indices[0] - 1] == letter) ^ (word[indices[1] - 1] == letter):  # Used exclusive or as our condition is only true when exactly one of them is true
             count += 1
     f.close()
     return count
