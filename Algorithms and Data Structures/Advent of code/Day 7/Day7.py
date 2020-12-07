ef78f0acf71f7d1eab1c5e642bca5a86d8cc4b31
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2020-12-07 15:09:28 +0400

    Day7 Commit: Added question, input and solution

---
+++ Algorithms and Data Structures/Advent of code/Day 7/Day7.py
@@ -0,0 +1,35 @@
+#1
+def search(fileName, word):
+    f = open(fileName, "r")
+    output = ""
+    for line in f:
+        line_split = line.split(" bags contain ")
+        if word in line_split[1]:
+            output += line_split[0] + "-" + search(fileName, line_split[0])
+    f.close()
+    return output
+
+#2
+def search_num_bags(fileName, word):
+    count = 0
+    f = open(fileName, "r")
+    for line in f:
+        line_split = line.split(" bags contain ")
+        if word in line_split:
+            line_split[1] = line_split[1].replace('.\n', '')
+            print(line_split[1])
+            if "no other bags" in line_split[1]:
+                return 0
+            for str in line_split[1].split(", "):
+                if str[-1] == 's':
+                    count += int(str[0])+ int(str[0]) * search_num_bags("input.txt", str[2:-5])
+                else:
+                    count += int(str[0])+ int(str[0]) * search_num_bags("input.txt", str[2:-4])
+    f.close()
+    return count
+
+
+# str = search("input.txt", "shiny gold")
+# print(len(set(str.split("-"))))
+
+print(search_num_bags('input.txt', 'shiny gold'))
